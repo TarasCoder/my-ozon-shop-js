cf2d8d4193dbc311fcd40135079ed9e7dcc2c52c
TarasCoder <4kaskad@gmail.com>
2021-12-21 11:34:05 +0200

    adding filter by category

--- src/modules/search.js
+++ src/modules/search.js
@@ -1,6 +1,6 @@
 import getData from "./getData";
 import renderGoods from "./renderGoods";
-import {searchFilter} from "./filters"
+import { searchFilter } from "./filters";
 
 const search = () => {
   const searchInput = document.querySelector(".search-wrapper_input");
