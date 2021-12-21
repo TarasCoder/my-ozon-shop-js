1e9c7600f64d1465b425f8b3f1a651dfa53d2166
TarasCoder <4kaskad@gmail.com>
2021-12-21 15:49:21 +0200

    adding filtering functionality by price and action checkbox

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import cart from "./modules/cart";
 import load from "./modules/load";
 import search from "./modules/search";
 import catalog from "./modules/catalog";
+import filter from "./modules/filter";
 
 cart();
 load();
 search();
 catalog();
+filter();
+
+// Homework
+// - 1 Реализовать фильтрацию товаров по цене
